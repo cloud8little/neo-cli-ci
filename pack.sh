5f2431ed6662a74dfe6d5bf0b1e72a7e97aabefa
cloud8little <34291844+cloud8little@users.noreply.github.com>
2019-05-10 11:13:24 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+PATH=$(pwd)
 CLIVERSION="v2.10.2-preview3"
-PLUGINS=("SimplePolicy@v2.10.1" "ImportBlocks@v2.10.1")
+# release - will use the zip package from neo web portal
+# local - will use the tar.gz file from local file under cloud8little: neo-cli-ci/neo-cli.tar.gz
+NEO_CLI_OPTION="release"
+# install - will install plugins when start neo-cli;
+# local - will use the Plugins file from local file under cloud8little: neo-cli-cli/Plugins;
+PLUGINS_OPTION="install"
+
+PLUGINS=("SimplePolicy@v2.10.1" "ImportBlocks@v2.10.1" "RpcWallet@v2.10.1")
 # download neo-cli
 if [[ $CLIVERSION =~ "preview" ]] 
 then
@@ -23,11 +31,22 @@ mkdir Plugins
 # install plugins
 # https://github.com/neo-project/neo-plugins/releases/download/v2.10.1/ApplicationLogs.zip
 
-for plugin in ${PLUGINS[@]} 
-do
-    name=${plugin%%@*}
-    version=${plugin##*@}
-    wget "https://github.com/neo-project/neo-plugins/releases/download/"${version}"/"${name}".zip"
-    unzip ${name}.zip
-    rm ${name}.zip
-done
+if [[ $PLUGINS_OPTION == "install" ]]
+then
+    for plugin in ${PLUGINS[@]} 
+    do
+        name=${plugin%%@*}
+        version=${plugin##*@}
+        wget "https://github.com/neo-project/neo-plugins/releases/download/"${version}"/"${name}".zip"
+        unzip ${name}.zip
+        rm ${name}.zip
+    done
+else
+    dir=$(ls -l $PATH |awk '/^d/ {print $NF}')
+    for file in $dir
+    do
+        if $file == "Plugins"
+            cp -r $PATH/Plugins/. Plugins
+        fi
+    done   
+fi
